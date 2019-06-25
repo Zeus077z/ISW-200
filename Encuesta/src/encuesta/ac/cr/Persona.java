c7da2bf39784f9a662648713690c9a0aa12b9d91
Daniel Rojas <jrojasm@est.utn.ac.cr>
2019-06-25 09:53:27 -0500

    Modificacion del toString de la clase Persona.

---
+++ Encuesta/src/encuesta/ac/cr/Persona.java
@@ -0,0 +1,67 @@
+package encuesta.ac.cr;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Daniel
+ */
+public class Persona {
+    protected String genero;
+    public static int PersonasTrabajan;
+    public static int PersonasNoTrabajan;
+    protected static int CantPersonas;
+    protected static int feme;
+    protected static int mascu;
+    protected static int femeT;
+    protected static int mascuT;
+
+    public static int getCantPersonas() {
+        return CantPersonas;
+    }
+
+    public static void setCantPersonas(int CantPersonas) {
+        Persona.CantPersonas = CantPersonas;
+    }
+     
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public int getPersonasTrabajan() {
+        return PersonasTrabajan;
+    }
+
+    public void setPersonasTrabajan(int PersonasTrabajan) {
+        this.PersonasTrabajan = PersonasTrabajan;
+    }
+
+    public int getPersonasNoTrabajan() {
+        return PersonasNoTrabajan;
+    }
+
+    public void setPersonasNoTrabajan(int PersonasNoTrabajan) {
+        this.PersonasNoTrabajan = PersonasNoTrabajan;
+    }
+    
+    
+    @Override
+    public String toString() {
+        return "Persona{" + "genero: " + genero + ", personas: " +Persona.CantPersonas + ", PersonasTrabajan: " + PersonasTrabajan + ", PersonasNoTrabajan: " + PersonasNoTrabajan + '}';
+    }
+    
+
+  
+       
+   
+    
+
+    
+}
---
+++ Encuesta/src/encuesta/ac/cr/Trabajo.java
@@ -0,0 +1,46 @@
+package encuesta.ac.cr;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Daniel
+ */
+public class Trabajo extends Persona {
+    private int horas;
+    private int rangoSalario;
+
+  
+
+    public int getRangoSalario() {
+        return rangoSalario;
+    }
+
+    public void setRangoSalario(int rangoSalario) {
+        this.rangoSalario = rangoSalario;
+    }
+
+    public int getHoras() {
+        return horas;
+    }
+
+    public void setHoras(int horas) {
+        this.horas = horas;
+    }
+
+    @Override
+    public String toString() {
+        String info = String.format("%s, Horas %d, Salario %d",super.toString(),horas,rangoSalario);
+        return info;
+    }
+    
+    
+
+    
+    
+    
+}
